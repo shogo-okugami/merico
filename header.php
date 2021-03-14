9bc22fd5534f7611952b543835378df4f159436c
shogo-okugami <okugami365@gmail.com>
2021-03-14 18:26:19 +0900

    modified:ヘッダー右部分修正 ログイン時にはアイコン画像 未ログイン時はログインのリンク

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <header class="l-header js-header">
   <div class="l-header__container">
     <h1 class="l-header__title"><a href="index.php">merico</a></h1>
-    <?php if (basename($_SERVER['PHP_SELF']) === 'index.php') : ?>
-      <form action="" name="" method="get" class="js-search-form">
+    <?php if (basename($_SERVER['PHP_SELF']) === 'index.php' || 'productIndex.php') : ?>
+      <form action="productIndex.php" name="" method="get" class="js-search-form">
         <input class="c-form__input js-search-value" type="text" name="word" value="<?php echo getFormData('word', true); ?>" placeholder="キーワードを入力">
         <div class="c-btn--submit js-search-button">
           <img class="search" src="img/search-icon.svg">
@@ -10,14 +10,20 @@
         </div>
       </form>
     <?php endif; ?>
-    <div>
-      <?php if (empty($_SESSION['user_id'])) : ?>
-        <div class="u-mb20"><a href="login.php" class="c-btn--primary">ログイン</a></div>
-        <div><a href="signup.php" class="c-btn--primary">ユーザー登録</a></div>
-      <?php else : ?>
-        <div class="u-mb20"><a href="mypage.php" class="c-btn--primary">マイページ</a></div>
-        <div><a href="logout.php" class="c-btn--primary">ログアウト</a></div>
-      <?php endif; ?>
-    </div>
+    <?php
+    //ログインしていない場合
+    if (!isLogin()) {
+    ?>
+      <div><a class="c-btn--login" href="login.php">ログイン</a></div>
+    <?php
+    //ログインしている場合はユーザーのプロフィール画像を表示する
+     } else {
+      $userInfo = getUser($_SESSION['user_id']);
+    ?>
+      <div class="p-profile__img--small">
+        <img src="<?php echo sanitize(showProfImg($userInfo['pic'])); ?>" alt="プロフィール画像" />
+        <a class="u-extendLink" href="mypage.php"></a>
+      </div>
+    <?php } ?>
   </div>
 </header>
