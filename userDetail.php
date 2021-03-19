d5d20671b6f61f023cf0fd3b260aa2f132ebeffc
shogo-okugami <okugami365@gmail.com>
2021-03-19 23:46:49 +0900

    modified:$userInfoを$sellerInfoに変更

--- userDetail.php
+++ userDetail.php
@@ -16,16 +16,16 @@ debugLogStart();
 // 商品IDのGETパラメータを取得
 $userId = (!empty($_GET['user_id'])) ? $_GET['user_id'] : '';
 // DBからユーザーデータを取得
-$userInfo = getUser($userId);
+$sellerInfo = getUser($userId);
 //DBから商品データを取得
 $productData = getMyProduct($userId);
 
 // パラメータに不正な値が入っているかチェック
-if (empty($userInfo)) {
+if (empty($sellerInfo)) {
   error_log('エラー発生:指定ページに不正な値が入りました');
   header("Location:home.php"); //トップページへ
 }
-debug('取得したユーザーデータ：' . print_r($viewData, true));
+debug('取得したユーザーデータ：' . print_r($userInfo, true));
 ?>
 <?php
 $siteTitle = 'ユーザープロフィール';
@@ -41,18 +41,18 @@ require('head.php');
       <div class="c-container">
         <div class="p-profile">
           <div class="p-profile__img">
-            <img src="<?php echo showProfImg($userInfo['pic']); ?>" alt="プロフィール画像">
+            <img src="<?php echo showProfImg($sellerInfo['pic']); ?>" alt="プロフィール画像">
           </div>
           <div class="p-profile__name">
-            <?php if (!empty($userInfo['name'])) echo sanitize($userInfo['name']); ?>
+            <?php if (!empty($sellerInfo['name'])) echo sanitize($sellerInfo['name']); ?>
           </div>
           <div class="p-profile__intro">
-            <p class="p-profile__intro__text <?php if (empty($userInfo['comment'])) echo 'u-color--gray'; ?>">
-              <?php echo (!empty($userInfo['comment'])) ? sanitize($userInfo['comment']) : 'コメントはありません。'; ?>
+            <p class="p-profile__intro__text <?php if (empty($sellerInfo['comment'])) echo 'u-color--gray'; ?>">
+              <?php echo (!empty($sellerInfo['comment'])) ? sanitize($sellerInfo['comment']) : 'コメントはありません。'; ?>
             </p>
           </div>
           <div class="p-profile__heading">
-            <?php echo sanitize($userInfo['name']) . 'さんが出品している商品'; ?>
+            <?php echo sanitize($sellerInfo['name']) . 'さんが出品している商品'; ?>
           </div>
           <div class="p-profile__list">
             <?php
@@ -63,10 +63,10 @@ require('head.php');
                   <a href="productDetail.php?product_id=<?php echo sanitize($val['id']); ?>" class="u-extendLink"></a>
                   <div class="c-panel__img">
                     <img src="<?php echo sanitize($val['pic1']); ?>" alt="<?php echo sanitize($val['name']); ?>">
+                    <p class="c-panel__price">¥<?php echo sanitize(number_format($val['price'])); ?></p>
                   </div>
                   <div class="c-panel__body">
                     <p class="c-panel__title"><?php echo sanitize($val['name']); ?></p>
-                    <p class="c-panel__price">¥<?php echo sanitize(number_format($val['price'])); ?></p>
                   </div>
                 </div>
               <?php
